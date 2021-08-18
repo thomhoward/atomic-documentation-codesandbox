6d66ae43d444053de364fd6c1c931499034f4080
Danny Lutz <dlutz@coveo.com>
2021-08-18 09:49:41 -0500

    remove extra script element

--- .github/scripts/update_package.rb
+++ .github/scripts/update_package.rb
@@ -25,11 +25,16 @@ class UpdatePackage
     def update_index_page
       version_stripped = @version.gsub(/(\d+\.)(\d+)(\.\d+)/,'v\1\2' )
       doc = @index_page
-      script = doc.xpath("//script").first
+      script_cdn = doc.xpath("//script").first
       stylesheet = doc.xpath("//link").first
-      script['src'] = "https://static.cloud.coveo.com/atomic/" + "#{version_stripped}" + "/atomic.esm.js"
+      script_cdn['src'] = "https://static.cloud.coveo.com/atomic/" + "#{version_stripped}" + "/atomic.esm.js"
       # at the time of making this, stylesheets are not being versioned in CDN so latest is always safest bet
       stylesheet['href'] = "https://static.cloud.coveo.com/atomic/latest/themes/default.css"
+      doc.xpath("//script").each do |script|
+        if script.to_s == '<script nomodule src="/build/atomic.js"></script>'
+          script.remove
+        end
+      end
       doc.to_html
       File.write("../../index.html", doc)
     end
